23e429523e743a0555ab4548f5164fb606247588
“Asparukh” <asparuh661@gmail.com>
2021-02-11 22:21:52 +0600

    init

---
+++ VariablesNames.py
@@ -0,0 +1,20 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal variable names
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# Camel case
+# myVariableName = "John"
+
+# Pascal case
+# MyVariableName = "John"
+
+# Snake Case
+# my_variable_name = "John"
---
+++ Variablesgetthetype.py
@@ -0,0 +1,8 @@
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+a = 4
+A = "Sally"
+#A will not overwrite a
